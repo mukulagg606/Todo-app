65a1e7372d78dad2dfce957a273e0a38a938b758
Mukul <mukulaggarwal606@gmail.com>
2023-04-11 10:59:28 +0530

    Created A ToDo-App

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+port = 8000;
+const expressLayout = require('express-ejs-layouts');
+app.use(expressLayout);
+const db = require('./config/mongoose');
+
+// it is use to handle middle ware here we are using express.urlencode to use the parser
+app.use(express.urlencoded({extended:false})) 
+app.use('/',require('./routes'))
+app.use(express.static('./assets')) // for getting static files
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true)
+app.set('view engine','ejs');
+app.set('views','./views')
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`error in running the ${port}`)
+        return;
+    }
+    console.log(`Server is running on port:${port}`)
+})
